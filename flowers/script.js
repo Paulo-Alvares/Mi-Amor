3abacc76fb6fbe3957b007cecd2b370bb6a0d097
Paulo-Alvares <paulo.alvares@sptech.school>
2024-06-15 16:53:43 -0300

    fix: add card to flowers

--- flowers/script.js
+++ flowers/script.js
@@ -4,3 +4,14 @@ onload = () => {
     clearTimeout(c);
   }, 1000);
 };
+
+const showCardButton = document.querySelector(".show-card");
+const cardContainer = document.querySelector(".card");
+
+showCardButton.addEventListener("click", () => {
+  if (cardContainer.style.display === "block") {
+    cardContainer.style.display = "none";
+  } else {
+    cardContainer.style.display = "block";
+  }
+});
